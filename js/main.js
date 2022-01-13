0cd85633c865db71a5e2a1d833ce1d42919e904c
Caitlin Lim <caitlim777@gmail.com>
2022-01-13 10:35:12 -0500

    implemented progress bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("collapsible--expanded");
+//   })
+// );
+
+document.addEventListener("DOMContentLoaded", function() {
+  const progressbarinner = document.querySelector('.progress-bar-inner');
+
+  window.addEventListener('scroll', function(){
+    let h = document.documentElement;
+    let st = h.scrollTop || document.body.scrollTop;
+    let sh = h.scrollHeight || document.body.scrollHeight;
+
+    let percent = st/(sh-h.clientHeight) * 100;
+    let roundedPercent = Math.round(percent);
+    
+    progressbarinner.style.width = percent + "%";
+    // progressbarinner.innerText = roundedPercent + "%";
+  });
+});
